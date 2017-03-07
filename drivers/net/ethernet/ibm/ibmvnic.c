f5675203b622447dba243059d775699ec37a5cd8
Nathan Fontenot <nfont@linux.vnet.ibm.com>
2017-03-07 15:30:37 -0500

    ibmvnic: Free scrqs when releasing sub crqs
    The tx/rx scrqs should be free'ed when we release the tx/rx sub crqs.
    
    Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -1314,6 +1314,7 @@ static void release_sub_crqs(struct ibmvnic_adapter *adapter)
 				release_sub_crq_queue(adapter,
 						      adapter->tx_scrq[i]);
 			}
+		kfree(adapter->tx_scrq);
 		adapter->tx_scrq = NULL;
 	}
 
@@ -1326,6 +1327,7 @@ static void release_sub_crqs(struct ibmvnic_adapter *adapter)
 				release_sub_crq_queue(adapter,
 						      adapter->rx_scrq[i]);
 			}
+		kfree(adapter->rx_scrq);
 		adapter->rx_scrq = NULL;
 	}
 }
