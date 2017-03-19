2197cfc594555361faec2472f3df285e3cc6a987
Nathan Fontenot <nfont@linux.vnet.ibm.com>
2017-03-19 02:54:09 -0400

    ibmvnic: Clean up the failure paths for ibmvnic_open
    Now that we can call ibmvnic_release_resources() multiple times,
    clean up the failure handling for ibmvnic_open to just call
    release resources.
    
    Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -574,13 +574,25 @@ static int ibmvnic_login(struct net_device *netdev)
 	return 0;
 }
 
+static void ibmvnic_release_resources(struct ibmvnic_adapter *adapter)
+{
+	ibmvnic_release_bounce_buffer(adapter);
+	ibmvnic_release_tx_pools(adapter);
+	ibmvnic_release_rx_pools(adapter);
+
+	ibmvnic_release_sub_crqs(adapter);
+
+	if (!adapter->failover && !adapter->needs_reset)
+		ibmvnic_release_crq_queue(adapter);
+
+	ibmvnic_release_stats_token(adapter);
+}
+
 static int ibmvnic_open(struct net_device *netdev)
 {
 	struct ibmvnic_adapter *adapter = netdev_priv(netdev);
 	struct device *dev = &adapter->vdev->dev;
 	union ibmvnic_crq crq;
-	int rxadd_subcrqs;
-	int tx_subcrqs;
 	int rc = 0;
 	int i;
 
@@ -607,16 +619,11 @@ static int ibmvnic_open(struct net_device *netdev)
 		return -1;
 	}
 
-	rxadd_subcrqs =
-	    be32_to_cpu(adapter->login_rsp_buf->num_rxadd_subcrqs);
-	tx_subcrqs =
-	    be32_to_cpu(adapter->login_rsp_buf->num_txsubm_subcrqs);
-
 	adapter->map_id = 1;
 	adapter->napi = kcalloc(adapter->req_rx_queues,
 				sizeof(struct napi_struct), GFP_KERNEL);
 	if (!adapter->napi)
-		goto alloc_napi_failed;
+		goto ibmvnic_open_fail;
 	for (i = 0; i < adapter->req_rx_queues; i++) {
 		netif_napi_add(netdev, &adapter->napi[i], ibmvnic_poll,
 			       NAPI_POLL_WEIGHT);
@@ -627,15 +634,15 @@ static int ibmvnic_open(struct net_device *netdev)
 
 	rc = ibmvnic_init_rx_pools(netdev);
 	if (rc)
-		goto rx_pool_failed;
+		goto ibmvnic_open_fail;
 
 	rc = ibmvnic_init_tx_pools(netdev);
 	if (rc)
-		goto tx_pool_failed;
+		goto ibmvnic_open_fail;
 
 	rc = ibmvnic_init_bounce_buffer(netdev);
 	if (rc)
-		goto bounce_init_failed;
+		goto ibmvnic_open_fail;
 
 	replenish_pools(adapter);
 
@@ -660,31 +667,12 @@ static int ibmvnic_open(struct net_device *netdev)
 
 	return 0;
 
-bounce_init_failed:
-	i = tx_subcrqs - 1;
-	kfree(adapter->tx_pool[i].free_map);
-tx_pool_failed:
-	i = rxadd_subcrqs;
-rx_pool_failed:
+ibmvnic_open_fail:
 	for (i = 0; i < adapter->req_rx_queues; i++)
 		napi_disable(&adapter->napi[i]);
-alloc_napi_failed:
-	ibmvnic_release_sub_crqs(adapter);
+
+	ibmvnic_release_resources(adapter);
 	return -ENOMEM;
-}
-
-static void ibmvnic_release_resources(struct ibmvnic_adapter *adapter)
-{
-	ibmvnic_release_bounce_buffer(adapter);
-	ibmvnic_release_tx_pools(adapter);
-	ibmvnic_release_rx_pools(adapter);
-
-	ibmvnic_release_sub_crqs(adapter);
-
-	if (!adapter->failover && !adapter->needs_reset)
-		ibmvnic_release_crq_queue(adapter);
-
-	ibmvnic_release_stats_token(adapter);
 }
 
 static int ibmvnic_close(struct net_device *netdev)
